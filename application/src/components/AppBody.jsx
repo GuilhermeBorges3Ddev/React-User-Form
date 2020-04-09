6c60dc48ff694657c5fca31c8ff07e6488340bf3
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-04-09 13:09:18 -0300

    Project completly responsive

--- application/src/components/AppBody.jsx
+++ application/src/components/AppBody.jsx
@@ -8,15 +8,15 @@ import MailContractButton from './subcomponents/MailContractButton';
 
 export default function AppBody() {
     return (
-        <Form className="d-flex w-75 h-100">
+        <Form className="d-flex justify-content-center align-tems-center w-75 h-100 p-0 m-0">
             <Row>
-                <Col sm="12" md="12" className="text-center">
+                <Col sm="12" md="12" lg="12" className="text-center">
                     <InputNames />
                 </Col>
-                <Col sm="12" md="12" className="text-center">
+                <Col sm="12" md="12" lg="12" className="text-center">
                     <GenderBirthPhone />
                 </Col>
-                <Col sm="12" md="12" className="text-center">
+                <Col sm="12" md="12" lg="12" className="text-center">
                     <MailContractButton />
                 </Col>
             </Row>
