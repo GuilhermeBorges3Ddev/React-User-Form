7dd7b8d1add135db2ed11d168f67532f3a662d1d
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-04-09 15:44:00 -0300

    Adding random class to update build version

---
+++ application/src/components/AppBody.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Form, Row, Col} from 'reactstrap';
+
+import '../styles/App.css';
+import InputNames from './subcomponents/InputNames';
+import GenderBirthPhone from './subcomponents/GenderBirthPhone';
+import MailContractButton from './subcomponents/MailContractButton';
+
+export default function AppBody() {
+    return (
+        <Form className="d-flex justify-content-center align-tems-center w-75 h-100 p-0 m-0 darkMode">
+            <Row>
+                <Col sm="12" md="12" lg="12" className="text-center">
+                    <InputNames />
+                </Col>
+                <Col sm="12" md="12" lg="12" className="text-center">
+                    <GenderBirthPhone />
+                </Col>
+                <Col sm="12" md="12" lg="12" className="text-center">
+                    <MailContractButton />
+                </Col>
+            </Row>
+        </Form>
+    )
+}
+
